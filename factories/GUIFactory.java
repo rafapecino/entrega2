3895acff4acb1e4b23cc5911c7ab9b52388ead4e
rafapecino <113851878+rafapecino@users.noreply.github.com>
2023-02-09 18:27:12 +0000

    copiado

---
+++ factories/GUIFactory.java
@@ -0,0 +1,5 @@
+
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
---
+++ factories/MacOSFactory.java
@@ -0,0 +1,13 @@
+
+public class MacOSFactory implements GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return new MacOSButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacOSCheckbox();
+    }
+}
